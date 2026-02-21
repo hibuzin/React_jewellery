4ffbc417c8ab1e7d41fb2762c4a32f7505c258f6
askar <hibuzin@gmail.com>
2026-02-21 07:42:40 +0530

    updated

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); // your backend URL
+const socket = io('https://jewellery-backend-icja.onrender.com'); // your backend URL
 
 export default socket;
